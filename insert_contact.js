17094ab4c34c62ca4d77548fcb50539ccbb7cc2e
yihaowang <yihao.wang@goanimate.com>
2017-12-27 01:38:16 +0800

    Parse Sheet

--- insert_contact.js
+++ insert_contact.js
@@ -4,6 +4,15 @@ const XLSX = require('xlsx');
 // Constant
 const FILE_NAME = 'test.xlsx';
 const VALID_SHEET_NAME = new Set(['捐款人']);
+const HEADER_LOOKUP = {
+    '中文全名': 'name',
+    'nickname': 'nickname',
+    '地址': 'address',
+    'email': 'email',
+    '紙本賀卡': 'paperCard',
+    '年度報告': 'annualReport',
+    '年度收據': 'annualReceipt',
+};
 
 function sheet2arr(sheet) {
     var result = [];
@@ -36,6 +45,55 @@ function sheet2arr(sheet) {
     return result;
 }
 
+function getIndexFromHeader(row) {
+    let result = {};
+
+    for (var key in HEADER_LOOKUP) {
+        const index = row.indexOf(key);
+
+        if (index === -1) {
+            return false;
+        }
+
+        result[HEADER_LOOKUP[key]] = index;
+    }
+
+    return result;
+}
+
+function parseRow(row, indexLookup) {
+    let result = {};
+
+    for (var key in indexLookup) {
+        const index = indexLookup[key];
+
+        result[key] = row[index];
+    }
+
+    return result;
+}
+
+function parseSheet(ws) {
+    const rows = sheet2arr(ws);
+    const numRow = rows.length;
+    let indexLookup,
+        result = [];
+
+    for (var i = 0; i < numRow; i++) {
+        const row = rows[i];
+
+        indexLookup = indexLookup || getIndexFromHeader(row);
+
+        if (!indexLookup) {
+            continue;
+        }
+
+        result.push(parseRow(row, indexLookup));
+    }
+
+    return result;
+}
+
 const wb = XLSX.readFile(FILE_NAME);
 
 for (var sheetName in wb.Sheets) {
@@ -43,7 +101,7 @@ for (var sheetName in wb.Sheets) {
         continue;
     }
 
-    const ws = sheet2arr(wb.Sheets[sheetName]);
+    const result = parseSheet(wb.Sheets[sheetName]);
 
-    console.log(ws);
+    console.log(result);
 }
